b2947194cd4e56159af17f85e6a664c2a88582f3
satyam400 <73168665+satyam400@users.noreply.github.com>
2021-02-08 22:22:57 +0530

    Delete atm.py

--- atm.py
+++
@@ -1,8 +0,0 @@
-x,bank_balance = [eval(x) for x in input().split()]
-if x%5==0:
-    if x+0.50<bank_balance:
-        print(format(bank_balance-(x+0.50),".2f"))
-    else:
-        print(format(bank_balance,".2f"))
-else:
-    print(format(bank_balance,".2f"))
